6b26e985cbb15140e9df6f0e2645554afd342925
Derek Shao <shao.derek.now@gmail.com>
2017-04-04 09:23:21 -0400

    postfix evaluator

--- src/test/Main.java
+++ src/test/Main.java
@@ -1,5 +1,7 @@
 package test;
 
+import java.lang.reflect.Array;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Stack;
@@ -10,30 +12,15 @@ public class Main {
 	
 	static HashMap<String, Integer> prec = new HashMap<String, Integer>();
 	
-
+	
 	public static void main(String[] args) {
 		
-		
-		//test case
 		String infix = "2+3*9-7";
 		String [] infixString = infix.split("(?!^)");
 		
 		Stack<String> stack= new Stack<String>();
 		String postfix = "";
 		
-		/*
-		Operator multiply = new Operator("*", 4);
-		Operator divide = new Operator("/", 3);
-		Operator add = new Operator("+", 2);
-		Operator subtract = new Operator("-", 1);
-        
-        
-		prec.put(multiply, multiply.getPrec());
-		prec.put(divide, divide.getPrec());
-		prec.put(add, add.getPrec());
-		prec.put(subtract, subtract.getPrec());
-		
-		*/
 		prec.put("*", 4);
 		prec.put("/", 3);
 		prec.put("+", 2);
@@ -67,21 +54,63 @@ public class Main {
         	postfix = postfix.concat(stack.pop()); 
         }
         
-
-        System.out.println("reached end of stack");
         System.out.println("postfix: " + postfix);
+        
+        //postfix evaluator
+        
+        Stack<String> post = new Stack<String>();
+        
+        String expression = ""; 
+        
+        String [] postfixString = postfix.split("(?!^)");
+        
+        for (String n : postfixString) {
+        	if (prec.containsKey(n)) {
+        		
+        		String topStack = post.peek();
+        		post.pop();
+        		
+        		int retVal = 0;
+        		
+        		switch(n) { 
+        			case "*":
+        				retVal = Integer.parseInt(post.pop()) * Integer.parseInt(topStack);
+        				break;
+        			case "+":
+        				retVal = Integer.parseInt(post.pop()) + Integer.parseInt(topStack);
+        				break;
+        			case "-":
+        				retVal = Integer.parseInt(post.pop()) - Integer.parseInt(topStack);
+        				break;
+        			case "/": 
+        				retVal = Integer.parseInt(post.pop()) / Integer.parseInt(topStack);
+        				break;
+        			default:
+        				break;
+        		}
+        		
+        		post.push(Integer.toString(retVal)); 
+        		
+        		System.out.print("Current stack" + Arrays.toString(post.toArray()));
+        		System.out.println(" ");
+        		
+        	}
+        	else {
+        		//operand
+        		post.push(n);
+        	}
+        }
+        
+        System.out.println("evaluated: " + post.peek());
+        
+        
 
 	}//main
 	
 	private static boolean isHigher(String c, String topStack) {
 		
-		System.out.println("**** IsHigher *****");
-		System.out.println(" topStack key: " + topStack + " topStack value: " + prec.get(topStack));
-		System.out.println(" c key: " + c + " c value: " + prec.get(c));
-				
 		return prec.get(topStack) > prec.get(c);
 	}
-	
 	
 	public static void printStack(Stack stack) {
 		
